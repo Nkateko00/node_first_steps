26b9be168900a95fb7a27623e332627cce25698a
Nkateko00 <tekonku@icloud.com>
2020-08-04 01:41:22 +0200

    all

--- hello-world.js
+++ hello-world.js
@@ -4,6 +4,7 @@ figlet('Goodnight Teko!!', function(err, data) {
     if (err) {
         console.log('Something went wrong...');
         console.dir(err);
+        var sam = "text";
         return;
     }
     console.log(data)
