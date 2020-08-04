ef93895cde04f34f982f1b3b9d97c47292650d07
Nkateko00 <tekonku@icloud.com>
2020-08-04 10:01:19 +0200

    initial

--- hello-world.js
+++ hello-world.js
@@ -1,6 +1,6 @@
 var figlet = require('figlet');
  
-figlet('Good Morning Teko!!', function(err, data) {
+figlet('Hello World!!', function(err, data) {
     if (err) {
         console.log('Something went wrong...');
         console.dir(err);
